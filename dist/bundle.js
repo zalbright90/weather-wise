cb422c7e49a82082cdf29a06219f74a627d913f1
zalbright90 <zalbright1990@hotmail.com>
2024-10-29 22:34:50 -0700

    Initial project set-up

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello, WeatherWise App!');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
